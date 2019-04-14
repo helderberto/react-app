956f3854d2c2b4f276ba435c653349ebaf5fd0e9
Helder Burato Berto <helder.burato@gmail.com>
2019-04-14 20:15:34 -0300

    fix: render 404 when not found

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 import BaseTemplate from '_templates/base'
 import HomePage from '_pages/home'
@@ -7,8 +7,10 @@ import NotFoundPage from '_pages/not-found'
 
 const Routes = () => (
   <Router component={BaseTemplate}>
-    <Route exact path="/" component={HomePage} />
-    <Route path="/404" component={NotFoundPage} />
+    <Switch>
+      <Route path="/" exact component={HomePage} />
+      <Route component={NotFoundPage} />
+    </Switch>
   </Router>
 )
 
